571a809e6c21d2e0f5776ef516c73750ab18a93c
whoisthereitsme <200156665+whoisthereitsme@users.noreply.github.com>
2026-01-29 05:22:28 +0100

    Add world/__init__.py

---
+++ world/__init__.py
@@ -0,0 +1,16 @@
+from .rows import ROWS, rows
+from .row import ROW
+from .materials import MATERIALS, Materials, Material
+
+from utils import *
+from world import *
+from bundle import *
+
+__all__ = [
+    "MATERIALS",
+    "Materials",
+    "Material",
+    "ROW",
+    "ROWS",
+    "rows",
+]
