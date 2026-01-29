b035bddbd268e7f42c3ba806dd42f6d98a2ff182
whoisthereitsme <200156665+whoisthereitsme@users.noreply.github.com>
2026-01-29 05:22:15 +0100

    Add _old/config.py

---
+++ _old/config.py
@@ -0,0 +1,50 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Config:
+
+
+    FPS_TGT = 60.0
+    SIDEPAD = 1024
+
+    MINEX = 1
+    MINEY = 1
+    MINEZ = 128
+
+    MERGETIME = 0.1
+
+    MATERIALS = {
+        "AIR": 0,
+        "STONE": 1,
+        "WATER": 2,
+    }
+
+    AXIS = {
+        "X": 0, "x": 0,
+        "Y": 1, "y": 1,
+        "Z": 2, "z": 2,
+    }
+
