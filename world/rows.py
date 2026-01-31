145fd85324d06a8c4c545115e6de418ee1ecff74
whoisthereitsme <200156665+whoisthereitsme@users.noreply.github.com>
2026-01-31 03:06:08 +0100

    Publish project snapshot

--- world/rows.py
+++ world/rows.py
@@ -182,7 +182,7 @@ class ROWS:
                     arids[mid_new] += 1
 
         self.remove(row=row)
-        self.bvh.fixupwards(node=self.bvh.parent[self.bvh.lidx[(mat0, rid)]])
+        self.bvh.fixupwards(node=self.bvh.root)
         return array, arids
 
     def split1(self, pos:POS=None, mat:str=None) -> ARRAY_ARIDS:
